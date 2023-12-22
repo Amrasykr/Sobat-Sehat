094fc1dc77950b713fd66e5492d2e9792061b3a0
Ammar Asysyakur <114901699+Amrasykr@users.noreply.github.com>
2023-12-22 18:08:38 +0700

    migration setup
    adding users table
    adding news table with relation
    adding activities table with relation

---
+++ database/migrations/2023_12_22_102804_create_activities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum(
+                'category',
+                ['kesehatan', 'olahraga', ]
+            );
+            $table->longtext('description');
+            $table->string('location');
+            $table->string('location_link');
+            $table->string('image')->default('activities.jpg');
+            $table->date('activity_date');
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
