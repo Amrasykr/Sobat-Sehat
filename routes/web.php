70adb4e19d177a02b9ce776b282da21f6c458ff6
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2023-12-23 11:07:41 +0700

    add daisy ui

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name("home");
+})->name('home');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,23 @@ export default {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        extend: {},
+    },
+
+    plugins: [require("daisyui")],
+
+    daisyui: {
+        themes: [
+            {
+                SobatSehat: {
+                    ...require("daisyui/src/theming/themes")["light"],
+                    "primary": "#06B6D4",
+                    "secondary": "#4ADE80",
+                }
+            },
+            "light", "dark", "cupcake"],
+    },
+
 }
 
